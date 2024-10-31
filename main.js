7db3947497aa55bb5235027053ddbffaa60280b2
Alexey917 <pochtovii.1996@mail.ru>
2024-10-31 23:18:28 +0300

    fixed input with keyboard_main-operation

--- main.js
+++ main.js
@@ -5,17 +5,28 @@ console.log(input.value);
 keyboard.forEach((btn) => {
   btn.addEventListener("click", () => {
     input.focus();
-    input.value == 0
+    
+    input.value == 0 && btn.classList.contains("keyboard_number")
       ? (input.value = btn.textContent)
       : (input.value += btn.textContent);
 
-    // if (btn.classList.contains("keyboard_main-operation")) {
-    //   input.value = btn.textContent;
-    // }
     if (btn.textContent === "C") input.value = 0;
-    // if (input.value[length - 1] === /+-%\/*/) {
 
-    // }
+    strTemp = input.value;
+    mainOperation = /[+-/*%]{2,}/g;
+
+    if (strTemp.match(mainOperation)) {
+      strTemp = strTemp.replace(mainOperation, btn.textContent);
+      input.value = strTemp;
+    }
+
+    if (btn.classList.contains("keyboard_erase")) {
+      console.log(strTemp.length);
+      // strTemp.slice(0, strTemp.length - 1);
+    }
+
+    console.log(input.value);
+
   });
 });
 
