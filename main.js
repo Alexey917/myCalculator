a04b3e0be8a97baf44690c8e28b90ce4ae9f1df4
Alexey917 <pochtovii.1996@mail.ru>
2024-11-02 20:43:09 +0300

    Implemented deletion of the previous character. Added calculation and display of simple results

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 const input = document.querySelector(".display__input");
 const keyboard = document.querySelectorAll(".keyboard__btn");
-console.log(input.value);
+const displayResult = document.querySelector(".display__result");
+
+const calculator = {
+  lOperand: "",
+  rOperand: "",
+
+  "+": (lOperand, rOperand) => +lOperand + +rOperand,
+  "-": (lOperand, rOperand) => +lOperand - +rOperand,
+  "*": (lOperand, rOperand) => +lOperand * +rOperand,
+  "/": (lOperand, rOperand) => +lOperand / +rOperand,
+  "%": (lOperand, rOperand) => +lOperand % +rOperand,
+};
 
 keyboard.forEach((btn) => {
   btn.addEventListener("click", () => {
     input.focus();
-    
+
     input.value == 0 && btn.classList.contains("keyboard_number")
       ? (input.value = btn.textContent)
       : (input.value += btn.textContent);
 
     if (btn.textContent === "C") input.value = 0;
+
+    if (btn.classList.contains("keyboard_erase")) {
+      input.value = "";
+      input.value = strTemp.slice(0, strTemp.length - 1);
+    }
 
     strTemp = input.value;
     mainOperation = /[+-/*%]{2,}/g;
@@ -20,13 +36,26 @@ keyboard.forEach((btn) => {
       input.value = strTemp;
     }
 
-    if (btn.classList.contains("keyboard_erase")) {
-      console.log(strTemp.length);
-      // strTemp.slice(0, strTemp.length - 1);
+    if (btn.textContent === "=") {
+      strTemp = strTemp.slice(0, strTemp.length - 1);
+      mas = strTemp.split(/[+-/*%]/g);
+      console.log(mas);
+      calculator.lOperand = mas[0];
+      calculator.rOperand = mas[1];
+      input.value = strTemp;
+
+      let result;
+
+      for (let ch of strTemp) {
+        console.log(strTemp);
+        if (Object.keys(calculator).includes(ch))
+          result = calculator[ch](calculator.lOperand, calculator.rOperand);
+      }
+      displayResult.textContent = "=" + result;
+      console.log(result);
     }
 
-    console.log(input.value);
-
+    // console.log(input.value);
   });
 });
 
